3cbdd472be572bbf26c1aff59c97cd659de0b5fb
Jacob Sipple <sipple3@gmail.com>
2019-04-22 14:07:38 -0400

    added react-animations and used it to shake the images when incorrect guess made

--- memory/src/components/game/Game.js
+++ memory/src/components/game/Game.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './Game.css'
+import {headShake} from 'react-animations';
+import {StyleSheet, css} from 'aphrodite';
+
 import jasnah from './images/jasnah.jpg'
 import Kaladin from './images/Kaladin.jpg'
 import kaladin2 from './images/kaladin2.jpg'
@@ -18,12 +21,20 @@ let image = []
 const images  = [jasnah, Kaladin, kaladin2, keisler, sazed, shai, shalash, Shallan, szeth, vasher, vin, vin2, vin3, vin4]
 
 
+const styles = StyleSheet.create({
+ headShake: {
+  animationName: headShake,
+  animationDuration: '1s'
+ }
+})
+
 class Game extends Component {
  constructor(props) {
   super(props)
   this.state = {
    images: [],
-   guessed: []
+   guessed: [],
+   shouldShake: false
   }
  }
  componentDidMount = () => {
@@ -50,15 +61,17 @@ class Game extends Component {
     guessed: []
    })
    this.props.incorrect()
+   this.setState({shouldShake: true}, () => {
+    const that = this;
+    setTimeout(() => that.setState({shouldShake: false}), 1000);
+});
   }
  }
  render() {
-  image = this.state.images.map( (x, i) => <img onClick={this.guess} id={i} className='image' src={x} alt='character' />)
-
+  image = this.state.images.map( (x, i) => <img additionalStyles={{text: {}, frame: {}}} frameClass={this.state.shouldShake ? css(styles.headShake) : ''} onClick={this.guess} id='image' className={this.state.shouldShake ? css(styles.headShake) : ''} src={x} alt='character' />)
   return( 
    <div>
    {image}
-   <img src={jasnah} alt='vin' />
    </div>
   )
  }
